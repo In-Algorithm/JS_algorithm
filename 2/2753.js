513c00b124ebb040baa6b4eea94caa71c394b466
borobong2 <borobong230@gmail.com>
2022-11-26 08:49:29 +0900

    221126 TMA

--- 2/2753.js
+++ 2/2753.js
@@ -2,11 +2,11 @@ var fs = require("fs");
 var input = fs.readFileSync("input.txt").toString().trim();
 
 const year = Number(input);
-const condition1 = year % 100 === 0;
-const condition2 = year % 4 === 0;
+const condition1 = year % 4 === 0;
+const condition2 = year % 100 !== 0;
 const condition3 = year % 400 === 0;
 
-if (condition1 && condition2 && condition3) {
+if (condition1 && (condition2 || condition3)) {
   console.log("1");
 } else {
   console.log("0");
