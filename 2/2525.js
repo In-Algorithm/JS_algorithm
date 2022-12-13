bfbadc3335740a985c1ef5a8fb19a5247e4fd52a
borobong2 <borobong230@gmail.com>
2022-12-13 21:00:30 +0900

    221213 TMA

--- 2/2525.js
+++ 2/2525.js
@@ -9,22 +9,21 @@ var hour = Number(input[0]);
 var minute = Number(input[1]);
 var timer = Number(fs.readFileSync("input.txt").toString().split("\n")[1]);
 let leftTime = minute + timer;
-let result_hour = -1;
-let result_minute = -1;
+let result_hour = 0;
+let result_minute = 0;
 let count = 0;
+
+if (leftTime < 60) {
+  return console.log(hour, minute + timer);
+}
 
 do {
   count++;
   result_hour += 1;
-  result_minute = 60 * count - (minute + timer);
+  result_minute = minute + timer - 60 * count;
   leftTime -= 60;
 } while (leftTime >= 60);
-
-if (result_hour !== -1 && result_minute !== -1) {
-  console.log(
-    hour + result_hour >= 24 ? hour + result_hour - 24 : hour + result_hour,
-    result_minute
-  );
-} else {
-  console.log(hour, minute + timer);
-}
+console.log(
+  hour + result_hour >= 24 ? hour + result_hour - 24 : hour + result_hour,
+  result_minute
+);
