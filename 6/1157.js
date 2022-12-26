f513806ec2dfaf232a272e981126e065c19ee4cd
borobong2 <borobong230@gmail.com>
2022-12-27 08:17:31 +0900

    221227 TMA

--- 6/1157.js
+++ 6/1157.js
@@ -4,6 +4,19 @@ const input = require("fs")
   .trim()
   .split("");
 let arr = [];
-for (let i of input) {
-  console.log(i);
+for (let i = 0; i < 26; i++) {
+  arr[i] = 0;
 }
+for (let i of input) {
+  index = i.charCodeAt() - 65;
+  if (i.charCodeAt() >= 97) {
+    index = i.charCodeAt() - 97;
+  }
+  arr[index] += 1;
+}
+const max = Math.max.apply(null, arr);
+const maxNum = arr.indexOf(max);
+const hasMaxNumTwice = arr.indexOf(max, maxNum + 1);
+hasMaxNumTwice !== -1
+  ? console.log("?")
+  : console.log(String.fromCharCode(maxNum + 65));
