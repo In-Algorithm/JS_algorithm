58df1b7cee5bbca87e59cf2a15224c23a73ccb71
borobong2 <borobong230@gmail.com>
2022-12-31 22:04:38 +0900

    221231 TMA:

---
+++ 6/2941.js
@@ -0,0 +1,16 @@
+let input = require("fs").readFileSync("dev/stdin").toString().trim();
+
+let replaced_str = input;
+let croatiaAlphabet = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="];
+let count = 0;
+for (let i of croatiaAlphabet) {
+  if (input.includes(i)) {
+    const letter = i;
+    const regexAllCase = new RegExp(letter, "g");
+    const counts = replaced_str.split(i).length - 1;
+    replaced_str = replaced_str.replace(regexAllCase, "");
+    count += counts;
+  }
+}
+count += replaced_str.length;
+console.log(count);
