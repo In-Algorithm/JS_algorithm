bf0ead88378cbdbdab8c6d7197188bd71c10a3e2
Bong <borobong230@gmail.com>
2023-01-24 15:06:17 +0900

    230124 TMA

--- greedy/11399.js
+++ greedy/11399.js
@@ -1,27 +1,14 @@
-const input = require("fs")
+const [n, input] = require("fs")
   .readFileSync("dev/stdin")
   .toString()
   .trim()
   .split("\n");
 
-const line = input[1].split(" ").map(Number);
-const sortLine = line.sort((a, b) => a - b);
-let ans = 0;
+const sortedArr = input.split(" ").sort((a, b) => a - b);
+let answer = 0;
 let prev = 0;
-
-sortLine.forEach((l) => {
-  prev += l;
-  ans += prev;
+sortedArr.forEach((value) => {
+  prev += Number(value);
+  answer += prev;
 });
-
-console.log(ans);
-
-// const fs = require("fs");
-// const [n, input] = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-
-// const answer = input
-//   .split(" ")
-//   .sort((a, b) => a - b)
-//   .reduce((acc, cur, i) => acc + cur * (n - i), 0);
-
-// console.log(answer);
+console.log(answer);
