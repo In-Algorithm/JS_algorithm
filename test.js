322e2a83d1574d2495609217f93101635ffac015
Bong <borobong230@gmail.com>
2023-01-28 05:01:24 +0900

    230128 TMA

--- test.js
+++ test.js
@@ -1,20 +1,13 @@
-const foo = () => {
-  setTimeout(() => {
-    throw Error("foo에서 발생한 에러");
-  }, 500);
-};
-const bar = () => {
-  foo();
-};
-
-const baz = () => {
-  bar();
-};
-
-try {
-  baz();
-} catch (err) {
-  console.error(err);
-  console.log("done");
-  console.log("test");
+const amounts = [1000.0, 2000.0, 3000.0];
+let input1 = 1;
+let input2 = 2;
+let input3 = 3;
+for (let amount of amounts) {
+  const input = [input1, input2, input3];
+  console.log(input[0]);
+  // let outstanding = 0;
+  // outstanding += input[count - 1] + amount * count;
+  // this.printOutstanding(count, outstanding);
+  // count++;
+  // sumOutstanding += outstanding;
 }
