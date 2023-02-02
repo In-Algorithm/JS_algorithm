cad79fe08863ce662fa5655f869befabd05244c9
Bong <borobong230@gmail.com>
2023-02-02 21:16:34 +0900

    230202 TMA

---
+++ greedy/10610.js
@@ -0,0 +1,18 @@
+const input = require("fs").readFileSync("dev/stdin").toString().trim();
+
+let arr = input.split("").sort((a, b) => b - a);
+let answer = "";
+
+if (!arr.includes("0")) {
+  return console.log(-1);
+}
+
+for (let i of arr) {
+  answer += i;
+}
+
+if (BigInt(answer) % BigInt(30) === BigInt(0)) {
+  console.log(answer);
+} else {
+  console.log(-1);
+}
