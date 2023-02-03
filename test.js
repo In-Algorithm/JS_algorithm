6bcd64fe75e6860d4b71956329f59948a71141ca
Bong <borobong230@gmail.com>
2023-02-03 20:34:55 +0900

    Change gitignore

--- test.js
+++ test.js
@@ -1,13 +1,42 @@
-const amounts = [1000.0, 2000.0, 3000.0];
-let input1 = 1;
-let input2 = 2;
-let input3 = 3;
-for (let amount of amounts) {
-  const input = [input1, input2, input3];
-  console.log(input[0]);
-  // let outstanding = 0;
-  // outstanding += input[count - 1] + amount * count;
-  // this.printOutstanding(count, outstanding);
-  // count++;
-  // sumOutstanding += outstanding;
-}
+// const a = () => {
+//   setTimeout(() => {
+//     throw new Error("error");
+//   }, 1000);
+// };
+
+// const b = () => {
+//   a();
+// };
+
+// const c = () => {
+//   b();
+// };
+
+// try {
+//   setTimeout(() => {
+//     console.log("byebye");
+//     throw new Error("error1");
+//   }, 1000);
+//   // c();
+//   // const a = new Error("error");
+//   // console.log(a);
+// } catch (e) {
+//   console.log("에러", e);
+// }
+
+const a = () => {
+  new Promise((resolve, reject) => {
+    reject("hihi");
+  });
+};
+
+(async () => {
+  try {
+    await a();
+  } catch (e) {
+    console.log("에러", e);
+  } finally {
+    console.log("33333");
+  }
+  console.log("byebye");
+})();
