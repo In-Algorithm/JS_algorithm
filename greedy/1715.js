5d9d95e11841ecefa1613c34ddc178139c6c6e99
Bong <borobong230@gmail.com>
2023-02-06 08:13:38 +0900

    230206 TMA

--- greedy/1715.js
+++ greedy/1715.js
@@ -2,20 +2,75 @@ const input = require("fs")
   .readFileSync("dev/stdin")
   .toString()
   .trim()
-  .split("\n")
-  .splice(1)
-  .sort((a, b) => a - b);
+  .split("\n");
 
-let answer = 0;
-const sol = (n, list) => {
-  for (let i = 0; i < n - 1; i++) {
-    let card1 = list.shift();
-    let card2 = list.shift();
-    let sum = Number(card1) + Number(card2);
-    answer += sum;
-    list.unshift(sum);
+class MinHeap {
+  constructor() {
+    this.heap = [];
   }
-};
-sol(input.length, input);
 
-console.log(answer);
+  getLength = () => {
+    return this.heap.length;
+  };
+
+  push = (node) => {
+    this.heap.push(node);
+    let i = this.heap.length - 1;
+    let parentI = Math.floor((i - 1) / 2);
+    while (i > 0 && this.heap[parentI] > this.heap[i]) {
+      this.swap(i, parentI);
+      i = parentI;
+      parentI = Math.floor((i - 1) / 2);
+    }
+  };
+
+  pop = () => {
+    if (this.heap.length === 1) {
+      return this.heap.pop();
+    }
+
+    const result = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    let i = 0;
+    while (true) {
+      const leftI = i * 2 + 1,
+        rightI = i * 2 + 2;
+      if (leftI >= this.heap.size) {
+        break;
+      }
+      let nextI = i;
+      if (this.heap[nextI] > this.heap[leftI]) {
+        nextI = leftI;
+      }
+      if (rightI < this.heap.length && this.heap[nextI] > this.heap[rightI]) {
+        nextI = rightI;
+      }
+      if (nextI === i) {
+        break;
+      }
+      this.swap(i, nextI);
+      i = nextI;
+    }
+    return result;
+  };
+
+  swap = (a, b) => {
+    const temp = this.heap[a];
+    this.heap[a] = this.heap[b];
+    this.heap[b] = temp;
+  };
+}
+
+const minHeap = new MinHeap();
+for (let i = 1; i < input.length; i++) {
+  minHeap.push(+input[i]);
+}
+let totalCompareCount = 0;
+while (minHeap.getLength() > 1) {
+  let aCount = minHeap.pop();
+  let bCount = minHeap.pop();
+  let compareCount = aCount + bCount;
+  totalCompareCount += compareCount;
+  minHeap.push(compareCount);
+}
+console.log(totalCompareCount);
