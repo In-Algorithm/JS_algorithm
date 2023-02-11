5b2f2eb20a058094d9801f2ed6240bb4baa69c3a
Bong <borobong230@gmail.com>
2023-02-11 13:54:07 +0900

    230211 TMA

--- greedy/1439.js
+++ greedy/1439.js
@@ -1,3 +1,25 @@
 const input = require("fs").readFileSync("dev/stdin").toString().trim();
 
-console.log(input);
+let arr = [];
+let element = "";
+
+for (let i = 0; i < input.length; i++) {
+  if (i === 0) {
+    element += input[0];
+  } else if (input[i] === input[i - 1]) {
+    element += input[i];
+  } else {
+    arr.push(element);
+    element = "";
+    element += input[i];
+  }
+  if (i === input.length - 1) {
+    arr.push(element);
+  }
+}
+let leng = arr.length;
+if (leng % 2 === 0) {
+  console.log(leng / 2);
+} else {
+  console.log((leng - 1) / 2);
+}
