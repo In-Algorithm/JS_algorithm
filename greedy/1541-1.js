fe4844d6ecafb4b8a19d1231b13d5b9ca7c891e3
Bong <borobong230@gmail.com>
2023-02-23 23:18:39 +0900

    230223 TMA

--- greedy/1541-1.js
+++ greedy/1541-1.js
@@ -1,3 +1,26 @@
 const input = require("fs").readFileSync("dev/stdin").toString().trim();
 
-console.log("test", input);
+const arr = input.toString().split("-");
+console.log("input", [input], arr, input == arr);
+
+let answer = 0;
+let isMinus = arr == input;
+for (let i of arr) {
+  if (i.includes("+") && !isMinus) {
+    let arr2 = i.toString().split("+");
+    for (let j of arr2) {
+      answer -= Number(j);
+    }
+    continue;
+  }
+  if (isMinus) {
+    let arr2 = i.toString().split("+");
+    for (let j of arr2) {
+      answer += Number(j);
+    }
+    continue;
+  }
+  answer += Number(i);
+  console.log("tt", answer);
+}
+console.log(answer);
