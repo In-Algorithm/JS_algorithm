58dc77872c22635de109a447c004857b00a203b0
Bong <borobong230@gmail.com>
2023-10-05 00:13:29 +0900

    feat: solve baekjoon 14929 by js

---
+++ prefixSum/14929.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const n = Number(input[0]);
+const nums = input[1].split(" ").map(Number);
+
+let total = nums.reduce((acc, curr) => acc + curr, 0);
+let result = 0;
+
+for (let num of nums) {
+  total -= num;
+  result += num * total;
+}
+
+console.log(result);
