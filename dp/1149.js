07a708f8cb4b8f4bbee68474d8390b0fa6a461ec
Bong <borobong230@gmail.com>
2023-10-28 23:15:38 +0900

    feat: Solve dp 1149 by js myself

--- dp/1149.js
+++ dp/1149.js
@@ -4,18 +4,16 @@ const input = require("fs")
   .trim()
   .split("\n");
 
-const length = Number(input[0]);
-const costs = input.slice(1).map((line) => line.split(" ").map(Number));
-const dp = Array.from({ length }, () => Array(3).fill(0));
-
+const len = Number(input.shift());
+const costs = input.map((v) => v.split(" ").map((v) => +v));
+const dp = Array.from({ length: len }, () => Array(3).fill(0));
 dp[0][0] = costs[0][0];
 dp[0][1] = costs[0][1];
 dp[0][2] = costs[0][2];
 
-for (let i = 1; i < length; i++) {
+for (let i = 1; i < len; i++) {
   dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + costs[i][0];
   dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + costs[i][1];
   dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + costs[i][2];
 }
-
-console.log(Math.min(...dp[length - 1]));
+console.log(Math.min(...dp[len - 1]));
